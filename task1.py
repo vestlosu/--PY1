512784ee7b056af46aad412a2a96fbe00a03b2fd
vestlosu <115497426+vestlosu@users.noreply.github.com>
2022-10-19 22:17:29 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+src = True and True or False and False
+src = True or False
+src = True
+# TODO расписать упрощение выражения
+
+result = True  # TODO подставить результат выражения
+
+print(src == result)
